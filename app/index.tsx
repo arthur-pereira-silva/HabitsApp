9e3013ccd0c23ef2f66d449726407c8a708f3420
calouros <calouros@fatec.corp>
2024-03-06 22:07:27 -0300

    alterações da aula

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
+/*import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import StyledInput from '../components/StyledInput';
 import StyledButton from '../components/StyledButton';
@@ -59,4 +59,51 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+});*/
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, View, Image } from 'react-native'
+import { router } from 'expo-router'
+import { Input, InputField, Text, Button, ButtonText } from '@gluestack-ui/themed'
+import { Entypo } from '@expo/vector-icons'
+
+export default function Login() {
+  const handleLogin = () => {
+    router.push('home')
+  }
+
+  return (
+    <View style={styles.container}>
+      
+      <Text size='sm'>Tela de login</Text>
+
+      <Input variant='outline' size='md' mb={8}>
+        <InputField placeholder='Digite o seu e-mail' />
+      </Input>
+
+      <Input variant='outline' size='md' mb={8}>
+        <InputField type='password' placeholder='Digite a sua senha' />
+      </Input>
+
+      <Button onPress={handleLogin} size='lg' variant='solid' action='primary'>
+        <ButtonText>Acessar o app <Entypo name="login" size={24} color="black" /></ButtonText>
+      </Button>
+
+      <StatusBar style="auto" />
+    </View>
+
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  tinyLogo:{
+    width: 100,
+    height: 100
+  }
 });
